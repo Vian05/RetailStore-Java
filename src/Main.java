9eb86c6fc7a07e15a574e6a73e21fb51a945b6b6
Vian05 <112783480+Vian05@users.noreply.github.com>
2026-09-11 22:18:45 +0800

    feat: add initial Main class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("   RETAIL STORE MANAGEMENT");
+        System.out.println("=================================");
+        System.out.println("Project setup berhasil.");
+    }
+}
