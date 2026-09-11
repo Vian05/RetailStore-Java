5fa17fc78c0814d452223074747438d7685fa040
Vian05 <112783480+Vian05@users.noreply.github.com>
2026-09-11 22:30:40 +0800

    chore: add reporting service skeleton

---
+++ src/ReportService.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ReportService {
+
+    public void showDailySales(ArrayList<Transaction> transactions) {
+        // TODO: Implement daily revenue and transaction count.
+    }
+
+    public void showLowStock(ArrayList<Product> products) {
+        // TODO: Implement low-stock report (stock < 10).
+    }
+
+    public void showTopThreeBestSelling(ArrayList<Transaction> transactions) {
+        // TODO: Implement Top 3 products by units sold.
+    }
+}
