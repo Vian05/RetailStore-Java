cdbd3d44dc69fadc4579767a490daa0c25d74645
Vian05 <112783480+Vian05@users.noreply.github.com>
2026-09-11 22:30:45 +0800

    chore: add discount skeleton

---
+++ src/Discount.java
@@ -0,0 +1,7 @@
+public class Discount {
+
+    public static double calculate(double subtotal) {
+        // TODO: Implement the discount business rule agreed by the team.
+        return 0.0;
+    }
+}
